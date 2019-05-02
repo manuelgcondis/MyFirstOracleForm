f397c0aae75e0c27e2daf8e93ae1c58acf1c92da
ManuelCondis <manuel_gutierrez@condis.es>
2019-05-02 12:20:26 +0200

    Sql Scripts Generation. Close #2

---
+++ scripts/InsertData.sql
@@ -0,0 +1,4 @@
+INSERT INTO customers_mg (customer_name, city) VALUES ('Rachel', 'New York');
+INSERT INTO customers_mg (customer_name, city) VALUES ('Oliver', 'Zurïch');
+INSERT INTO customers_mg (customer_name, city) VALUES ('Jack', 'Genève');
+INSERT INTO customers_mg (customer_name, city) VALUES ('Charlie', 'Seattle');
---
+++ scripts/PdbState.sql
@@ -0,0 +1,3 @@
+select name,open_mode from v$pdbs;
+-- alter pluggable database all open;
+-- alter pluggable database all save state;
